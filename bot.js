98adb66613516cd58bfe5294c27062e06ecdb7ad
CrazyBunny <41792186+Crazy8unny@users.noreply.github.com>
2019-01-08 18:20:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,11 +24,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
+        switch(message) {
             // !ping
             case 'big bang':
 				bot.sendMessage({
